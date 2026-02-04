0b6d35e77a47472e5daac6166f09b731e5c97b84
ilya2600 <ilya2600business@gmail.com>
2026-02-04 21:01:51 +0300

    Add files via upload

--- app.py
+++ app.py
@@ -6,18 +6,28 @@ app = Flask(__name__)
 
 db = sqlite3.connect("data.db")
 db.execute("CREATE TABLE IF NOT EXISTS clicks (count INTEGER)")
-db.execute("INSERT INTO clicks (count) VALUES (0)")
-rows = db.execute("SELECT * FROM clicks").fetchall()
-print(rows)
 
-count = 0
+rows = db.execute("SELECT * FROM clicks").fetchall()
+
+if len(rows) == 0:
+    db.execute("INSERT INTO clicks (count) VALUES (0)")
+# rows = db.execute("SELECT * FROM clicks").fetchall()
+# print(rows)
+db.commit()
+db.close()
+
 
 @app.route("/", methods=['GET','POST'])
 def index():
+    db = sqlite3.connect("data.db")
+    
     if request.method == "POST":
-        global count
-        count += 1
-    return render_template("index.html", count=count) 
+        db.execute("UPDATE clicks SET count = count + 1")
+        
+    rows = db.execute("SELECT * FROM clicks").fetchall()
+    db.commit()
+    db.close()
+    return render_template("index.html", rows=rows) 
     
 
 
